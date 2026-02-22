afda056fc4ae32551b13bdbebcf4a51573d44dc9
ARRY7686 <aadigupta2007@yahoo.com>
2026-02-22 19:26:31 +0530

    Completed Ex5

--- SOLID/Ex5/src/ExportResult.java
+++ SOLID/Ex5/src/ExportResult.java
@@ -1,9 +1,26 @@
 public class ExportResult {
+
     public final String contentType;
     public final byte[] bytes;
+    public final String errorMessage;
 
     public ExportResult(String contentType, byte[] bytes) {
         this.contentType = contentType;
         this.bytes = bytes;
+        this.errorMessage = null;
+    }
+
+    private ExportResult(String errorMessage) {
+        this.contentType = null;
+        this.bytes = new byte[0];
+        this.errorMessage = errorMessage;
+    }
+
+    public static ExportResult error(String message) {
+        return new ExportResult(message);
+    }
+
+    public boolean isSuccess() {
+        return errorMessage == null;
     }
 }
